31c7392ef1ec9f3e6e9bf5fc83c363212009a231
jashanjotsingh <s_jashan@encs.concordia.ca>
2019-09-22 15:34:07 -0400

    [cmake] refactoring and minimal cmake

--- include/cugl.h
+++ include/cugl.h
@@ -36,7 +36,19 @@
  * \todo bool isVisible(Point) - harder than it looks
  */
 
+#ifdef __APPLE__
+#include <OpenGL/gl.h>
+#include <OpenGL/glu.h>
+#include <GLUT/glut.h>
+#else
+#ifdef _WIN32
+  #include <windows.h>
+#endif
+#include <GL/gl.h>
 #include <GL/glu.h>
+#include <GL/glut.h>
+#endif
+
 #include <iostream>
 #include <cmath>
 
